c6212192808e872fb06ccc2344ea6e1f4644c3ca
jaemoon <woans5970@naver.com>
2023-12-23 00:58:14 +0900

    section 7: 회원 관리 예제 - AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,18 +20,9 @@ public class MemberService {
     }
     
     public Long join(Member member) {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member); // 중복 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); // 중복 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -42,15 +33,7 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMS = finish - start;
-            System.out.println("findMembers " + timeMS + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
