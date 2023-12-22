5f4ee61df2ef3447d855e550167e253b07399e93
jaemoon <woans5970@naver.com>
2023-12-23 00:02:55 +0900

    section 6: 회원 관리 예제 - JPA

--- src/main/java/hello/hellospring/springConfig.java
+++ src/main/java/hello/hellospring/springConfig.java
@@ -1,10 +1,8 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +12,11 @@ import javax.sql.DataSource;
 @Configuration
 public class springConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public springConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public springConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -30,6 +28,7 @@ public class springConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
